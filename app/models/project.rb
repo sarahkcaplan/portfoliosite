2832c50f63c1d770037163f5c62e8cb4589c2d0d
Sarah Caplan <caplan.sarah@gmail.com>
2017-11-26 19:09:41 -0800

    Project routes and controller

--- app/models/project.rb
+++ app/models/project.rb
@@ -3,4 +3,12 @@ class Project < ApplicationRecord
   has_many :technologies, through: :ratings
   has_many :ratings
 
+  def index
+    @projects = Project.all
+  end
+
+  def show
+    @project = Project.find(params[:id])
+  end
+
 end
